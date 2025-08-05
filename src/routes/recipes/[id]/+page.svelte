9d82571c6fe583dd2df9d66bfa508d02dc162924
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-08-05 21:08:26 +0300

    recipe page styling

--- src/routes/recipes/[id]/+page.svelte
+++ src/routes/recipes/[id]/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <header>
-	<h1>{data.name}</h1>
+	<h1 class="text-2xl font-bold">{data.name}</h1>
 	<a href="/">🏠↩️</a>
 </header>
 
@@ -20,20 +20,20 @@
 
 {#if data.notes}
 	<section class="liirumlaarum">
-		<h3>Liirum laarum</h3>
+		<h3 class="text-lg font-medium">Liirum laarum</h3>
 		{#each data.notes as note}
 			<p>{note}</p>
 		{/each}
 	</section>
 {/if}
 
-<main>
-	<section>
-		<h2>Ainehet</h2>
+<main class="flex">
+	<section class="m-3">
+		<h2 class="text-xl font-bold">Ainehet</h2>
 		<p>TODO: parsing sections etc.</p>
 	</section>
-	<section>
-		<h2>Tee näin</h2>
+	<section class="m-3">
+		<h2 class="text-xl font-bold">Tee näin</h2>
 		<ol>
 			{#each data.method as step}
 				<!-- TODO: implement strikethrough -->
@@ -44,23 +44,6 @@
 </main>
 
 <style>
-	header {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-	}
-
-	header > a {
-		text-decoration: None;
-	}
-
-	main {
-		width: 70%;
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
-	}
-
 	.done {
 		color: #aaaaaa;
 		text-decoration: line-through;
