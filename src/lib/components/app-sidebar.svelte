47ac9267112df074f9655893d4b04f0a769d31aa
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-08-05 16:53:25 +0300

    wrap with Menu to avoid bullets

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,28 +1,32 @@
 <script>
 	import CalendarIcon from '@lucide/svelte/icons/calendar';
 	import CookingPotIcon from '@lucide/svelte/icons/cooking-pot';
+	import PlusIcon from '@lucide/svelte/icons/file-plus-2';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 
 	const items = [
 		{ title: 'Recipes', url: '/', icon: CookingPotIcon },
+		{ title: 'New recipe', url: '/', icon: PlusIcon },
 		{ title: 'Meal planning', url: '/recipes/ankkaragu', icon: CalendarIcon }
 	];
 </script>
 
 <Sidebar.Root>
 	<Sidebar.Content>
-		{#each items as item (item.title)}
-			<Sidebar.MenuItem>
-				<Sidebar.MenuButton>
-					{#snippet child({ props })}
-						<a href={item.url} {...props}>
-							<item.icon />
-							<span>{item.title}</span>
-						</a>
-					{/snippet}
-				</Sidebar.MenuButton>
-			</Sidebar.MenuItem>
-		{/each}
+		<Sidebar.Menu>
+			{#each items as item (item.title)}
+				<Sidebar.MenuItem>
+					<Sidebar.MenuButton>
+						{#snippet child({ props })}
+							<a href={item.url} {...props}>
+								<item.icon />
+								<span>{item.title}</span>
+							</a>
+						{/snippet}
+					</Sidebar.MenuButton>
+				</Sidebar.MenuItem>
+			{/each}
+		</Sidebar.Menu>
 	</Sidebar.Content>
 	<Sidebar.Footer />
 </Sidebar.Root>
