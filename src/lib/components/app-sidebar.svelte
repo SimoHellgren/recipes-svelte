59960a6ae0e514eca67072069f9ed006d06928ec
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-08-06 09:18:18 +0300

    formatting and qol

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -5,30 +5,36 @@
 	import ShoppingBasketIcon from '@lucide/svelte/icons/shopping-basket';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 
+	//function for closing the sidebar when link is clicked
+	let actions = Sidebar.useSidebar();
+	const closeBar = () => actions.setOpen(false);
+
 	const items = [
 		{ title: 'Reseptit', url: '/', icon: CookingPotIcon },
-		{ title: 'Uusi', url: '/', icon: PlusIcon },
-		{ title: 'Suunnittelu', url: '/', icon: CalendarIcon },
-		{ title: 'Ostoskori', url: '/', icon: ShoppingBasketIcon }
+		{ title: 'Uusi', url: '#new', icon: PlusIcon },
+		{ title: 'Suunnittelu', url: '#plan', icon: CalendarIcon },
+		{ title: 'Ostoskori', url: '#basket', icon: ShoppingBasketIcon }
 	];
 </script>
 
-<Sidebar.Root>
-	<Sidebar.Content>
-		<Sidebar.Menu>
-			{#each items as item (item.title)}
-				<Sidebar.MenuItem>
-					<Sidebar.MenuButton>
-						{#snippet child({ props })}
-							<a href={item.url} {...props}>
-								<item.icon />
-								<span>{item.title}</span>
-							</a>
-						{/snippet}
-					</Sidebar.MenuButton>
-				</Sidebar.MenuItem>
-			{/each}
-		</Sidebar.Menu>
-	</Sidebar.Content>
+<Sidebar.Root variant="inset" collapsible="icon">
+	<Sidebar.Inset>
+		<Sidebar.Content>
+			<Sidebar.Menu>
+				{#each items as item (item.title)}
+					<Sidebar.MenuItem>
+						<Sidebar.MenuButton>
+							{#snippet child({ props })}
+								<a href={item.url} {...props} onclick={closeBar}>
+									<item.icon />
+									<span>{item.title}</span>
+								</a>
+							{/snippet}
+						</Sidebar.MenuButton>
+					</Sidebar.MenuItem>
+				{/each}
+			</Sidebar.Menu>
+		</Sidebar.Content>
+	</Sidebar.Inset>
 	<Sidebar.Footer />
 </Sidebar.Root>
