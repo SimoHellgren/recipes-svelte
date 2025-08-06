5995a97f4df0753ae06d864ba5885790528fc2a4
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-08-06 19:56:08 +0300

    visual feedback for deletion and alternative button to commit tag

--- src/lib/components/tags-input.svelte
+++ src/lib/components/tags-input.svelte
@@ -1,5 +1,6 @@
 <script>
 	import CrossIcon from '@lucide/svelte/icons/x';
+	import PlusIcon from '@lucide/svelte/icons/circle-plus';
 	import Badge from './ui/badge/badge.svelte';
 	import Input from './ui/input/input.svelte';
 
@@ -7,14 +8,25 @@
 
 	let inputValue = $state('');
 
+	function addTag() {
+		const tag = inputValue.trim();
+
+		//no empties
+		if (!tag) return undefined;
+
+		//no dupes
+		if (value.includes(tag)) return undefined;
+
+		value = [...value, tag];
+		inputValue = '';
+	}
+
 	function handleKeydown(event) {
 		//intercept enter to add tags
 		if (event.key === 'Enter') {
 			event.preventDefault();
-			if (inputValue && !value.includes(inputValue.trim())) {
-				value = [...value, inputValue.trim()];
-				inputValue = '';
-			}
+
+			addTag(inputValue);
 		}
 	}
 
@@ -23,21 +35,27 @@
 	}
 </script>
 
-<Input
-	bind:this={ref}
-	bind:value={inputValue}
-	placeholder="Type & hit enter to add tag"
-	onkeydown={handleKeydown}
-	{...restProps}
-/>
+<div class="flex gap-1">
+	<Input
+		bind:this={ref}
+		bind:value={inputValue}
+		placeholder="Type & hit enter to add tag"
+		onkeydown={handleKeydown}
+		{...restProps}
+	/>
+
+	<button type="button">
+		<PlusIcon onclick={addTag} />
+	</button>
+</div>
 
 {#each value as tag}
-	<Badge variant="secondary">
+	<Badge class="group" variant="secondary">
 		<span>
 			{tag}
 		</span>
 		<button type="button" class="flex place-items-center">
-			<CrossIcon class="size-4" onclick={() => removeTag(tag)} />
+			<CrossIcon class="size-4 group-hover:text-red-400" onclick={() => removeTag(tag)} />
 		</button>
 	</Badge>
 {/each}
