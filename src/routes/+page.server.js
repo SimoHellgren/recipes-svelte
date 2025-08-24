99693edf5276dd8b4504d9d1e86c2aad5062b882
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-08-24 10:40:40 +0300

    sort

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,7 +1,7 @@
 import { supabase } from "$lib/supabaseClient";
 
 export async function load() {
-    const { data } = await supabase.from("recipe").select();
+    const { data } = await supabase.from("recipe").select().order("name");
     return {
         recipes: data ?? [],
     };
