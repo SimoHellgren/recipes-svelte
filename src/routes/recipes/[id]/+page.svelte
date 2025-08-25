e892b575608a9fef5004ff6f2fa68a7c44e23e51
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-08-25 16:28:04 +0300

    scale recipe

--- src/routes/recipes/[id]/+page.svelte
+++ src/routes/recipes/[id]/+page.svelte
@@ -1,36 +1,62 @@
 <script>
 	import Badge from '$lib/components/ui/badge/badge.svelte';
 	import Checkbox from '$lib/components/ui/checkbox/checkbox.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
 	import Label from '$lib/components/ui/label/label.svelte';
 
 	let { data } = $props();
+
+	const original_qty = data.yield_quantity;
+	let scaled_quantity = $state(original_qty);
+
+	let recipe = $derived.by(() => {
+		let r = {
+			...data,
+			yield_quantity: scaled_quantity,
+			section: data.section.map((s) => ({
+				...s,
+				assembly: s.assembly.map((i) => ({
+					...i,
+					quantity: (i.quantity * scaled_quantity) / original_qty
+				}))
+			}))
+		};
+
+		return r;
+	});
 </script>
 
-<header>
-	<h1 class="text-2xl font-bold">{data.name}</h1>
-	<a href="/">🏠↩️</a>
-	{#each data.tags as tag}
+<header class="flex gap-x-2 text-center align-middle">
+	<h1 class="text-2xl font-bold">{recipe.name}</h1>
+	{#each recipe.tags as tag}
 		<Badge variant="secondary">
 			{tag}
 		</Badge>
 	{/each}
 </header>
 
-<p>
-	<b>Alkuperä</b>:
-	{#if data.source && data.source.startsWith('http')}
-		<a href={data.source}>{data.source}</a>
-	{:else}
-		{data.source}
-	{/if}
-</p>
+<div class="flex flex-col gap-y-3 py-2">
+	<p>
+		<b>Alkuperä</b>:
+		{#if recipe.source && recipe.source.startsWith('http')}
+			<a href={recipe.source}>{recipe.source}</a>
+		{:else}
+			{recipe.source}
+		{/if}
+	</p>
 
-<p><b>Riitto:</b> {data.yield_quantity} {data.yield_unit}</p>
-
-{#if data.notes}
+	<div class="w-s flex gap-x-3">
+		<span>
+			<b>Riitto:</b>
+			<input class="w-10" type="number" step="0.5" min="0" bind:value={scaled_quantity} />
+			{recipe.yield_unit}
+		</span>
+	</div>
+</div>
+{#if recipe.notes}
 	<section class="liirumlaarum">
 		<h3 class="text-lg font-medium">Liirum laarum</h3>
-		{#each data.notes as note}
+		{#each recipe.notes as note}
 			<p>{note}</p>
 		{/each}
 	</section>
@@ -40,7 +66,7 @@
 	<section class="m-3">
 		<h2 class="text-xl font-bold">Ainehet</h2>
 		<ul class="space-y-2.5">
-			{#each data.section as section}
+			{#each recipe.section as section}
 				{#if section.name}
 					<div class="font-bold">{section.name}</div>
 				{/if}
@@ -62,7 +88,7 @@
 	<section class="m-3">
 		<h2 class="text-xl font-bold">Tee näin</h2>
 		<ol class="space-y-2.5">
-			{#each data.method as step}
+			{#each recipe.method as step}
 				<li>
 					<Label
 						class="font-normal has-[[aria-checked=true]]:text-gray-400 has-[[aria-checked=true]]:line-through"
