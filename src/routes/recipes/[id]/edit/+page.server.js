97354d2c03f2bce0ca784986ec029694f467feb9
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-09-02 20:41:15 +0300

    left instructions for future me

--- src/routes/recipes/[id]/edit/+page.server.js
+++ src/routes/recipes/[id]/edit/+page.server.js
@@ -62,8 +62,24 @@ export const actions = {
             })
             .eq("id", form.data.id)
 
+        // TODO: create missing ingredients
+        // easiest is probably to just "get or create" all
 
-        console.log(recipeError)
+        // the order of operation is significant:
+        // 1. delete first: makes sure that no conflicts on position when updating rest
+        // 2. (update existing ones)
+        // 3. add new ones - because rest have been updated, there cannot be conflicts with position (I think) 
+
+        // need to work on how to identify which records were deleted, though. Naive approach is
+        // to query the db, but there's probably something nicer available. Might be a separate ticket, though. 
+
+        // TODO: delete sections (cascade)
+        // TODO: update sections
+        // TODO: add sections
+
+        // TODO: delete assembly rows (cascade)
+        // TODO: update assembly rows
+        // TODO: add new assembly rows
 
         // const { data, error } = await supabase.from("ingredient")
         //     .upsert([{ name: "Öljy", conversions: { "g": 90, "ml": 100 } }], { onConflict: "name" })
@@ -71,7 +87,8 @@ export const actions = {
 
 
 
-
+        // TODO: submmitting should probably redirect back to the recipe
+        // and display a success message
         return {
             form,
         };
