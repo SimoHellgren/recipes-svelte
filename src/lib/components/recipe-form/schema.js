e50724158016372fb24804263db793251d47596e
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-09-03 11:41:18 +0300

    light schema restructuring

--- src/lib/components/recipe-form/schema.js
+++ src/lib/components/recipe-form/schema.js
@@ -1,5 +1,31 @@
 import { optional, z } from 'zod';
 
+// TODO: is there a nicer way to reuse the defaults?
+const defaultIngredient = {
+    name: null,
+    quantity: null,
+    unit: null,
+    // comment: null, 
+    // optional: false,
+}
+
+const defaultSection = {
+    name: null,
+    ingredients: [defaultIngredient]
+}
+
+const ingredientSchema = z.object({
+    name: z.string(),
+    quantity: z.number(),
+    unit: z.string(),
+    // comment: z.nullable(z.string()),
+    // optional: z.boolean().default(false),
+}).default(defaultIngredient)
+
+const sectionSchema = z.object({
+    name: z.nullable(z.string()),
+    ingredients: z.array(ingredientSchema)
+}).default(defaultSection)
 
 export const formSchema = z.object({
     name: z.string(),
@@ -11,29 +37,9 @@ export const formSchema = z.object({
     method: z.string(),
     notes: z.nullable(z.string()),
     tags: z.array(z.string()),
-    sections: z.array(z.object({
-        name: z.nullable(z.string()),
-        ingredients: z.array(z.object({
-            name: z.string(),
-            quantity: z.number(),
-            unit: z.string(),
-            // comment: z.nullable(z.string()),
-            // optional: z.boolean().default(false),
-        }))
-    })).default([{
-        name: null,
-        ingredients: [
-            {
-                name: null,
-                quantity: null,
-                unit: null,
-                // comment: null, 
-                // optional: false,
-            }
-        ]
-    }])
+    sections: z.array(sectionSchema).default([defaultSection])
 })
 
 export const editSchema = formSchema.extend({
-    id: z.number()
+    id: z.number(),
 })
