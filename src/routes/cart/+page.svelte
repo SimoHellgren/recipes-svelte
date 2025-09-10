d16cc1302427d02add6618db6ec872b82fd42763
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-09-10 21:23:31 +0300

    scale recipes in cart

--- src/routes/cart/+page.svelte
+++ src/routes/cart/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Button from '$lib/components/ui/button/button.svelte';
+	import Label from '$lib/components/ui/label/label.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
 	import DatePicker from '$lib/components/date-picker.svelte';
 	import { getCartState } from '$lib/state.svelte';
@@ -9,10 +10,33 @@
 
 	const { ingredients } = data;
 
-	const recipesInCart = $derived(cart.items.map((x) => x.id));
-	const chosen = $derived(ingredients.filter((i) => recipesInCart.includes(i.recipe_id)));
+	const recipes = $derived(
+		cart.items.map((recipe) => ({
+			...recipe,
+			ingredients: ingredients.filter((i) => i.recipe_id === recipe.id)
+		}))
+	);
+
+	/**
+	 * Might need to use $effect to keep states in sync later.
+	 */
+	const scales = $state(Object.fromEntries(cart.items.map((r) => [r.id, r.yield_quantity])));
+
+	const scaledIngredients = $derived(
+		recipes
+			.map((r) =>
+				r.ingredients
+					.map((i) => ({
+						...i,
+						quantity: (i.quantity * scales[r.id]) / r.yield_quantity
+					}))
+					.flat()
+			)
+			.flat()
+	);
+
 	const grouped = $derived(
-		Object.groupBy(chosen, ({ ingredient, unit }) => `${ingredient} (${unit})`)
+		Object.groupBy(scaledIngredients, ({ ingredient, unit }) => `${ingredient} (${unit})`)
 	);
 
 	const emptyCart = () => {
@@ -26,6 +50,16 @@
 	<Input bind:value={cart.name} placeholder="(nimetön ostoskori)" />
 	<DatePicker bind:value={cart.date} />
 	<Button onclick={emptyCart}>Tyhjennä kori</Button>
+</div>
+
+<div class="grid grid-rows-1">
+	{#each recipes as recipe, i}
+		<Label>
+			{recipe.name}
+			<Input type="number" step="any" bind:value={scales[recipe.id]} />
+			{recipe.yield_unit}
+		</Label>
+	{/each}
 </div>
 
 <h1>Ostoslista</h1>
