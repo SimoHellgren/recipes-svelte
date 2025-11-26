8775374a7c692fb3d748829883209502e3e4dc76
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-11-26 19:16:10 +0200

    filter recipes

--- src/routes/ingredients/+page.svelte
+++ src/routes/ingredients/+page.svelte
@@ -4,16 +4,40 @@
 
 	let { data } = $props();
 	let { ingredients, recipes } = data;
+
+	let ingredientStates = $state(ingredients.map((i) => ({ ...i, checked: false })));
+	let selectedIngredients = $derived(ingredientStates.filter((i) => i.checked));
+
+	let selectedRecipes = $derived(
+		recipes.filter((recipe) =>
+			selectedIngredients.every((selectedIng) =>
+				recipe.ingredients.some((i) => i.id === selectedIng.id)
+			)
+		)
+	);
 </script>
 
-<div class="grid grid-cols-2 gap-24">
+<div class="grid grid-cols-3 gap-18">
 	<div>
 		<h1>Ainehet</h1>
 		<ul>
-			{#each ingredients as ingredient}
+			{#each ingredientStates.filter((i) => !i.checked) as ingredient}
 				<li>
 					<Label class="m-1 rounded-md border-1 border-neutral-900 p-1">
-						<Checkbox class="bg-neutral-50" />
+						<Checkbox bind:checked={ingredient.checked} class="bg-neutral-50" />
+						{ingredient.name}
+					</Label>
+				</li>
+			{/each}
+		</ul>
+	</div>
+	<div>
+		<h1>Valitut</h1>
+		<ul>
+			{#each ingredientStates.filter((i) => i.checked) as ingredient}
+				<li>
+					<Label class="m-1 rounded-md border-1 border-neutral-900 p-1">
+						<Checkbox bind:checked={ingredient.checked} class="bg-neutral-50" />
 						{ingredient.name}
 					</Label>
 				</li>
@@ -23,7 +47,7 @@
 	<div>
 		<h1>Reseptit</h1>
 		<ul>
-			{#each recipes as recipe}
+			{#each selectedRecipes as recipe}
 				<li>
 					{recipe.name}
 				</li>
