49e72384368b937315cfd17abdd19a26fdf8da94
Simo Hellgrén <simo.hellgren@relexsolutions.com>
2025-12-11 20:39:37 +0200

    extract update recipe

--- src/lib/service.js
+++ src/lib/service.js
@@ -73,3 +73,92 @@ export const createRecipe = async (supabase, data) => {
         }
     }
 }
+
+export const updateRecipe = async (supabase, data) => {
+    // update basic stuff 
+    // we're passing in the whole form here, and currently db.updateRecipe takes care of
+    // picking out the right stuff. This probably should be changed
+    const { data: recipeData, error: recipeError } = await db.updateRecipe(supabase, data)
+
+    // sections
+    // ignore ingredients for now
+    const sections = data.sections
+        .map((s, i) => ({
+            id: s.id,
+            name: s.name,
+            position: i + 1,
+            recipe_id: data.id,
+        }))
+
+    const sectionIds = sections.map(s => s.id).filter(s => s) // no nulls
+
+    // delete sections (cascades to assembly rows)
+    // retains the sections that still are present on the form
+    const { data: deletedSections } = await db.deleteSections(supabase, data.id, sectionIds)
+
+    // update sections. Upsert since it allows a list as input
+    const { data: updatedSections, error: updateSectionError } = await db.upsertSections(
+        supabase,
+        sections.filter(s => s.id) // no null ids, i.e. no new sections here
+    )
+
+    // add sections
+    const { data: insertedSections, error: insertError } = await db.createSections(
+        supabase,
+        sections.filter(s => s.id === null).map(s => ({
+            name: s.name,
+            position: s.position,
+            recipe_id: s.recipe_id,
+        })) // only new sections, and ignore ids
+    )
+
+    const allSections = [...updatedSections, ...insertedSections]
+
+    const ingredients = data.sections.map(s => s.ingredients).flat()
+
+    // Get / Create ingredients
+    const { data: allIngredients } = await db.getOrCreateIngredients(supabase, ingredients)
+
+    // delete assembly rows
+    const assemblyIds = ingredients.map(i => i.id).filter(i => i) // no nulls
+    const { data: deletedAssemblies } = await db.deleteAssemblies(supabase, sectionIds, assemblyIds)
+
+    // update assembly rows
+    const assemblyRecords = data.sections.map(section => section.ingredients.map((assembly, index) => ({
+        id: assembly.id,
+        section_id: allSections.find(s => s.name === section.name).id,
+        ingredient_id: allIngredients.find(i => i.name === assembly.name).id,
+        position: index + 1,
+        quantity: assembly.quantity,
+        unit: assembly.unit,
+        comment: assembly.comment,
+        optional: assembly.optional,
+    }))).flat()
+
+
+    const assembliesToUpdate = assemblyRecords.filter(x => x.id)
+    const assembliesToCreate = assemblyRecords.filter(x => !x.id).map(({ id, ...rest }) => rest) //drop null ids
+
+    // offset trick for positions because unique index
+    const BIG_NUMBER = 1000;
+    // stupid, but works because realistically we'll never hit the offset number
+    await db.upsertAssemblies(
+        supabase,
+        assembliesToUpdate.map(a => ({ ...a, position: a.position + BIG_NUMBER }))
+
+    )
+
+    // fix the positions
+    const { data: updatedAssemblies, error: updatedAssemblyError } = await db.upsertAssemblies(supabase, assembliesToUpdate)
+
+    const { data: createdAssemblies, error: createdAssemblyError } = await db.createAssemblies(
+        supabase,
+        assembliesToCreate.map(a => ({ ...a, position: a.position + BIG_NUMBER }))
+    )
+
+    // fix positions
+    const { data: fixedAssemblies, error: fixedAssemblyError } = await db.upsertAssemblies(
+        supabase,
+        createdAssemblies.map(a => ({ ...a, position: a.position - BIG_NUMBER }))
+    )
+}
